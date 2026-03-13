a36315fcfc520c8ed72e687deca22cb12290e3d8
Vivek Gangdhar <vivekgangdhar11@gmail.com>
2026-03-13 12:59:46 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+function add(a, b) {
+    return a + b;
+}
+
+const result = add(2,3);
+
+if(result === 5){
+    console.log("Test Passed");
+}else{
+    console.log("Test Failed");
+    process.exit(1);
+}
